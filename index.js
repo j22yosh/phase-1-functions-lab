a2a810b7dba4f1559789f3defff3537e89d3316c
Joshua <j22yosh@gmail.com>
2022-03-23 03:30:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+ function distanceFromHqInBlocks(something){
+    if(something > 42)  {
+    return (something - 42)}
+    else return (42- something)}
+
+    function distanceFromHqInFeet(something){
+    
+    if(something > 42)  {
+        return (something - 42) * 264}
+        else return (42- something) * 264}
+    
+function distanceTravelledInFeet(a,b){
+    if (a > b){
+        return (a-b) * 264
+
+    }
+    else return (b-a) *264
+
+}
+
+function calculatesFarePrice(a,b){
+   
+   
+    if (distanceTravelledInFeet(a,b) <= 400){
+       return 0
+   }
+   else if (distanceTravelledInFeet(a,b) <= 2000){
+    return 2.56
+   }
+   else if (distanceTravelledInFeet(a,b) <= 2500){
+       return 25}
+    else return "cannot travel that far"
+
+}
